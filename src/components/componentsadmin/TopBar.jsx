3bf69787874c8fa4e3be93aa6c4de5cfa46c692c
zeckyjulian <julianhimawan@gmail.com>
2025-08-30 23:07:06 +0700

    topbar

---
+++ src/components/componentsadmin/TopBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FiCalendar } from 'react-icons/fi'
+
+export const TopBar = () => {
+  return (
+    <div className='border-b px-4 mb-4 mt-4 pb-4 border-stone-400'>
+        <div className='flex items-center justify-between p-0.5'>
+            <div>
+                <span className='text-sm font-bold block'>
+                    Good Morning, Julian!
+                </span>
+                <span className='text-xs block text-stone-500'>
+                    Tuesday, Aug 30th 2025
+                </span>
+            </div>
+
+            <button className='flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded'>
+                <FiCalendar />
+                <span>Prev 6 Months</span>
+            </button>
+        </div>
+    </div>
+  )
+}
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { TopBar } from '../../components/componentsadmin/TopBar'
 
 export const Dashboard = () => {
   return (
-    <div className='bg-white rounded-lg pb-4 shadow h-[200vh]'></div>
+    <div className='bg-white rounded-lg pb-4 shadow h-[200vh]'>
+      <TopBar />
+    </div>
   )
 }
