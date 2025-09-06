386685aae0edc73aaaedde0c91d97e07d97cbf47
zeckyjulian <julianhimawan@gmail.com>
2025-09-06 22:32:12 +0700

    order api

---
+++ src/api/orders.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8000/api';
+
+export const getOrders = async () => {
+    const response = await axios.get(`${API_URL}/orders`, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    });
+    return response.data.data;
+};
+
+export const createOrder = async (paymentMethod, shippingAddress) => {
+    const response = await axios.post(`${API_URL}/orders`, {
+        payment_method: paymentMethod,
+        shipping_address: shippingAddress,
+    }, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    });
+    return response.data.data;
+};
+
+export const updateOrder = async (id, status) => {
+    const response = await axios.put(
+        `${API_URL}/orders/${id}/status`,
+        { status },
+        {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        },
+    );
+    return response.data.data;
+};
